dcfd13f9c9b0a42a31122c326bca58618a8eace5
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-02-13 21:59:52 +0530

    Added dynamic "No" button text with increasing urgency

--- src/App.tsx
+++ src/App.tsx
@@ -8,3 +8,27 @@ export default function Page() {
   const handleNoClick = () => {
     setNoCount(noCount + 1);
   };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "What if I asked really nicely?",
+      "Pretty please",
+      "With a chocolate rice cake on top",
+      "What about a matcha frostie",
+      "PLEASE POOKIE",
+      "But 😭",
+      "I am going to die",
+      "Yep im dead",
+      "ok you are  talking to lover's ghost",
+      "please babe",
+      ":((((",
+      "PRETTY PLEASE",
+      "Estoy muerto",
+      "बेबी कृपया स्वीकार करा",
+      "No :(",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
