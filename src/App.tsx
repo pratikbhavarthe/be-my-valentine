6af7c55b42c348a71376eb9fe2d3078a4fbedb30
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-02-13 22:40:47 +0530

    implement Valentine's Day interactive component

--- src/App.tsx
+++ src/App.tsx
@@ -32,3 +32,38 @@ export default function Page() {
 
     return phrases[Math.min(noCount, phrases.length - 1)];
   };
+
+  return (
+    <div className="-mt-16 flex h-screen flex-col items-center justify-center">
+      {yesPressed ? (
+        <>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <div className="my-4 text-4xl font-bold">HOOOOORAAYYYYY!!! I LOVE YOU POOOOKIEEE PIE 🥰🥹🥧🧸!! ;))</div>
+        </>
+      ) : (
+        <>
+          <img
+            className="h-[200px]"
+            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+          />
+          <h1 className="my-4 text-4xl">Be My Valentine?</h1>
+          <div className="flex items-center">
+            <button
+              className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              onClick={handleNoClick}
+              className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
